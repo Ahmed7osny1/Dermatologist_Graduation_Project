814e72989c9d1fb830674224e8532846cd3f40d0
Ahmed Hosny <ahmedhosny6688@gmail.com>
2022-12-08 22:21:24 +0200

    first task (home fragment)

--- app/src/main/java/com/autismagraduation/dermatologist/HomeFragment.kt
+++ app/src/main/java/com/autismagraduation/dermatologist/HomeFragment.kt
@@ -6,14 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Login : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 }
--- app/src/main/java/com/autismagraduation/dermatologist/SplashFragment.kt
+++ app/src/main/java/com/autismagraduation/dermatologist/SplashFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Login : Fragment() {
+class SplashFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
 }
