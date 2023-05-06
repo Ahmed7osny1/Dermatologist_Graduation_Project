e5f4f1e0f961dabd41693e8169f96b06b4356777
Ahmed Hosny <ahmedhosny6688@gmail.com>
2023-05-06 22:00:46 +0300

    Task 9 (Changes)

--- pharmacy/src/main/java/com/autismagraduation/pharmacy/ui/ForgetPasswordFragment.kt
+++ pharmacy/src/main/java/com/autismagraduation/pharmacy/ui/ForgetPasswordFragment.kt
@@ -1,19 +1,21 @@
-package com.autismagraduation.dermatologist.ui.appscreen
+package com.autismagraduation.pharmacy.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.autismagraduation.dermatologist.R
+import com.autismagraduation.pharmacy.R
 
-class ProfileFragment : Fragment() {
+
+class ForgetPasswordFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_forget_password, container, false)
     }
+
 }
--- pharmacy/src/main/java/com/autismagraduation/pharmacy/ui/ProfileFragment.kt
+++ pharmacy/src/main/java/com/autismagraduation/pharmacy/ui/ProfileFragment.kt
@@ -1,10 +1,11 @@
-package com.autismagraduation.pharmacy
+package com.autismagraduation.pharmacy.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.autismagraduation.pharmacy.R
 
 
 class ProfileFragment : Fragment() {
