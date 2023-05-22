82ad727749679fb8a48c7209f71c556648f037ce
Mayada Mohamed <81150206+MayadaMohamed12@users.noreply.github.com>
2023-05-22 03:15:00 +0300

    herbs

--- app/src/main/java/com/autismagraduation/dermatologist/object/SetDataHerbs.kt
+++ app/src/main/java/com/autismagraduation/dermatologist/object/SetDataHerbs.kt
@@ -11,17 +11,189 @@ object SetDataHerbs {
 
         herbsData.add(
             DataUsed(
-                R.drawable.hyalu,
-                "HYALU COLLAGEN",
-                "Eye Cream",
-                "\nTo moisturize the area around the eye and protect\n"+" it from wrinkles and lines resulting from the dryness of the area around the eye.\n\n"+
-                        "It also helps to lighten the circles around the eye and make the area around the eye brighter, livelier, and a more youthful appearance.\n\n" +
-                        "Hyalu Collagen Eye Moisturizing Cream is enriched with caffeine, hyaluronic acid and marine collagen.\n\n" +
-                        "I mean, it will be very suitable for moisturizing and nourishing an area under the eye, and resisting wrinkles, fine lines, dark circles and bloating.\n\n" +
-                        "I mean, if you suffer from circles, we will know the cause and treat it with the use of the cream, which will improve the shape of the circles due to caffeine.\n\n" +
-                        "And we can use it even if there are no circles for hydration and freshness due to hyaluronic acid.\n\n"
+                R.drawable.hrbeczema,
+                "Eczema",
+                "Apple cider vinegar Always dilute apple cider vinegar before applying it to the skin — undiluted vinegar can cause chemical burns or other injuries.\n" +
+                        "\n" +
+                        "People can use the vinegar in wet wraps or baths, and it is available in most supermarkets and health stores.",
+                "\nTo use apple cider vinegar in a wet wrap:\n" +
+                        "\n" +
+                        "Mix 1 cup of warm water and 1 tablespoon of apple cider vinegar.\n" +
+                        "Apply the solution to cotton or gauze.\n" +
+                        "Cover the dressing in clean cotton fabric.\n" +
+                        "Leave it on the area for 3 hours.\n" +
+                        "To try an apple cider vinegar bath soak:\n" +
+                        "\n" +
+                        "Add 2 cups of apple cider vinegar to a warm bath.\n" +
+                        "Soak for 15–20 minutes.\n" +
+                        "Rinse the body thoroughly.\n" +
+                        "Moisturize within several minutes of leaving the bath.\n\n"
             )
         )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbtinea,
+                "Tinea",
+                "Tinea Herbs",
+                "\nTURMERIC\n" +
+                        "Turmeric is an effective natural remedy for many skin conditions because of its antimicrobial and anti-inflammatory prowess. Its antifungal properties can help treat tinea versicolor and prevent the infection from spreading.\n" +
+                        "\n" +
+                        "Mix a teaspoon of turmeric powder with a little water and mix it into a nice, smooth paste. Apply the finished paste to the affected parts of your skin and leave it dry for at least 30 minutes before rinsing it off.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbnail,
+                "Nail Fungs",
+                "Nail Herbs",
+                "\nGinger\n" +
+                        "Ginger is powerful natural nail fungus killer because it contains gingerols and shogaols. These inflammation-fighting, antifungal components of the ginger root support a healthy balance of bacteria and help fight bad bacteria such as yeast and Candida albicans.\n" +
+                        "\n" +
+                        "If these natural nail fungus killers don’t seem to help get rid of your fungal infection, it likely means that your toenail fungus is a sign of an underlying infection such as Candida overgrowth.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbherbs,
+                "Herpes",
+                "Cool compress",
+                "\nIt may be helpful to use a cold compress to reduce swelling. Apply an ice pack or a clean, soft washcloth filled with ice to the affected area. Repeat as needed every 4 hours. Be sure to never place ice directly on the skin.\n" +
+                        "2-Baking soda paste\n" +
+                        "Applying a baking soda paste may help dry out lesions and relieve itching. To do this, dip a wet cotton ball into a small amount of pure baking soda and dab it on the sore.\n\n"+
+                        "3-Cornstarch paste\n" +
+                        "Corn starch paste may also dry out lesions and relieve itching. Dip a wet cotton ball into a small amount of cornstarch and apply it to the affected area.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbhairloss,
+                "Hair Loss",
+                "Ginkgo hair mask",
+                "\nIngredients:\n" +
+                        "1 tsp ginkgo biloba powder\n" +
+                        "1 chopped banana\n" +
+                        "2 tsp coconut oil\n" +
+                        "1 tsp honey\n" +
+                        "Steps:\n" +
+                        "Mix all the ingredients and apply this mask to your hair.\n" +
+                        "Leave it on for half an hour, and then rinse it off with a mild shampoo." +
+                        "2-Onions are widely acknowledged for their hair growing potential. They are loaded with sulfur, which is one of the most important hair-building nutrients. \n" +
+                        "\n" +
+                        "The juice extracted from onions contains concentrated amounts of sulfur, which when applied to the scalp helps stimulate hair follicles and promote hair regrowth. Moreover, onions are known to exhibit antibacterial properties that help fight against scalp infections.       " +
+                        "                   how to make it :                                                                                                                                                                                                                                  Grind one onion and squeeze out its juice.\n" +
+                        "Soak a cotton ball in the onion juice and dab it all over your scalp.\n" +
+                        "Let your scalp marinate in the juice for 20–30 minutes, and then wash it off using a mild shampoo and plain water.\n" +
+                        "Use this hair treatment once a week.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbhairloss,
+                "Hair Loss",
+                "Ginkgo hair mask",
+                "\nIngredients:\n" +
+                        "1 tsp ginkgo biloba powder\n" +
+                        "1 chopped banana\n" +
+                        "2 tsp coconut oil\n" +
+                        "1 tsp honey\n" +
+                        "Steps:\n" +
+                        "Mix all the ingredients and apply this mask to your hair.\n" +
+                        "Leave it on for half an hour, and then rinse it off with a mild shampoo." +
+                        "2-Onions are widely acknowledged for their hair growing potential. They are loaded with sulfur, which is one of the most important hair-building nutrients. \n" +
+                        "\n" +
+                        "The juice extracted from onions contains concentrated amounts of sulfur, which when applied to the scalp helps stimulate hair follicles and promote hair regrowth. Moreover, onions are known to exhibit antibacterial properties that help fight against scalp infections.       " +
+                        "                   how to make it :                                                                                                                                                                                                                                  Grind one onion and squeeze out its juice.\n" +
+                        "Soak a cotton ball in the onion juice and dab it all over your scalp.\n" +
+                        "Let your scalp marinate in the juice for 20–30 minutes, and then wash it off using a mild shampoo and plain water.\n" +
+                        "Use this hair treatment once a week.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbmigment,
+                "Pigment Disorder",
+                "Apple cider vinegar",
+                "\nApple cider vinegar contains acetic acid,  may lighten pigmentation.\n" +
+                        "\n" +
+                        "To use this remedy:\n" +
+                        "\n" +
+                        "Combine equal parts apple cider vinegar and water in a container.\n" +
+                        "Apply to your dark patches and leave on two to three minutes.\n" +
+                        "Rinse using lukewarm water.\n" +
+                        "Repeat twice daily you achieve the results you desire.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbonce,
+                "acne",
+                "Acne Herbs",
+                "\n1-honey\n" +
+                        "Using a clean finger or cotton pad, rub a little honey into pimples. Otherwise, add honey to a face or body mask\n\n"
+                        +"2-An individual can either drink green tea or put green tea extract on their skin\n"+
+                        "\n3-apple cider vinegar\n" +
+                        "Mix 1 part apple cider vinegar and 3 parts water (use more water for sensitive skin).\n" +
+                        "After cleansing, gently apply the mixture to the skin using a cotton ball.\n" +
+                        "Let it sit for 5 to 20 seconds, rinse with water and pat dry.\n" +
+                        "Repeat this process 1 to 2 times per day, as needed."+
+                        "\n4-Make a honey and cinnamon mask                                         \n" +
+                        "Mix 2 tablespoons of honey and 1 teaspoon of cinnamon to form a paste.\n" +
+                        "After cleansing, apply the mask to your face and leave it on for 10 to 15 minutes.\n" +
+                        "Rinse the mask off completely and pat your face dry."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbutr,
+                "Urticaria",
+                "Natural Remedy to Treat Skin Disorders",
+                "\nOatmeal\n" +
+                        "Cornstarch\n" +
+                        "Prepare a paste by adding two cups of oatmeal and one cup of cornstarch and add some water to the mixture and form a paste by mixing them. " +
+                        "Apply this paste over affected part and let it stay for 20 to 30 minutes. " +
+                        "Repeat this remedy twice in a day regularly to heal the welts on skin this is one of the best home remedies for urticaria."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbvas,
+                "vasculitis",
+                "Apple Cider Vinegar",
+                "\nACV is a wonderful natural remedy for many of the vasculitis symptoms, " +
+                        " The acetic and malic acids in ACV have antiviral, antibiotic and anti-fungal properties.\n\n"
+            )
+        )
+
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.hrbver,
+                "versicolor",
+                "Rakta Chandana",
+                "\nIt is a natural tan remover and has cooling properties on the skin. It is very active in treating blemishes, breakouts and burning sensation over the skin.\n" +
+                        "\n" +
+                        "It has proven significant in dealing with skin diseases. It also has antioxidant, antibacterial, anti-inflammatory and wound-healing properties.\n" +
+                        "\n" +
+                        "How to use it?\n" +
+                        "Rakta Chandan can be mixed with rose water or honey and can be applied to the skin directly. It can also be blended with Haridra (turmeric powder) to heal breakouts and wounds." +
+                        " It is highly recommended for tinea versicolor patches.\n\n"+
+                        "2-Lodhra\n" +
+                        "It is a natural anti-inflammatory herb and is used widely to treat various skin infections including tinea versicolor. It is used to prepare various capsules, tonics, for local application and wash and powder (churnas) due to its antioxidant, antiseptic, antifungal and anti-inflammatory properties.\n" +
+                        "\n" +
+                        "How to use it?\n" +
+                        "Lodhra can be used both internally and externally. Lodhra powder can be mixed with aloe vera gel or rose water and apply the paste on tinea versicolor patches to get treated instantly. This is one of the best Herbs for Tinea Versicolor.\n\n"
+            )
+        )
+
 
         return herbsData
     }
