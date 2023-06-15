62cded470cbadb76b8d18366ed0c764f50bc00cf
Mayada Mohamed <81150206+MayadaMohamed12@users.noreply.github.com>
2023-06-15 21:59:05 +0300

    herbs add

--- app/src/main/java/com/autismagraduation/dermatologist/object/SetDataHerbs.kt
+++ app/src/main/java/com/autismagraduation/dermatologist/object/SetDataHerbs.kt
@@ -11,11 +11,77 @@ object SetDataHerbs {
 
         herbsData.add(
             DataUsed(
+                R.drawable.burn,
+                "Burn",
+                "Turmeric is excellent for burn" ,
+                "\nThe antiseptic property of turmeric saves your burnt skin from getting infected." +
+                        " Burnt skins are prone to infection. Take a piece of raw turmeric and grind it to paste. Apply a thick layer of turmeric on your burnt skin. Repeat the process again and again. Everyday use of turmeric on burnt skin helps to cure burnt skin very quickly."+
+                        "Banana leaf is excellent for burnt skin. Take a piece of banana leaf and wash it properly. Now wrap your wound with this leave. The cool banana leaves helps in the speedy recovery of burnt skin.\n" +
+                        "\n" +
+                        "Follow these tips according to the instruction along with a healthy diet and soon cure your burn. Burns are prone to infection. So, be careful about that. If the problem gets complicated soon take the advice of a doctor."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.onych,
+                "Onychomycosis",
+                "Oregano Essential Oil" ,
+                "\nApply 3 drops of oregano oil on the affected nail.\n" +
+                        "Massage and spread well so that it is absorbed and allowed to dry.\n" +
+                        "Use twice a day until you notice improvement."+
+                        "\nThyme Essential Oil\n" +
+                        "It stands out for its thymol content, since it has powerful antimicrobial and anti-fungal properties that are effective against nail fungus and restores a more pleasant appearance.\n" +
+                        "\n" +
+                        "What should we do?\n" +
+                        "\n" +
+                        "Apply 3 drops of thyme oil on the affected area.\n" +
+                        "Spread well on the nail so that it is absorbed and let it dry.\n" +
+                        "Use two to three times a day, until you notice improvement."+
+                        "\nCinnamon Essential Oil\n" +
+                        "Cinnamon essential oil is very effective in killing nail fungus. Cinnamon is rich in eugenol, a very potent anti-fungal chemical compound. This keeps the nail area dry, which prevents the fungus from reproducing.\n" +
+                        "\n" +
+                        "What should we do?\n" +
+                        "\n" +
+                        "Apply 3 or 4 drops of cinnamon oil on the affected nail.\n" +
+                        "Spread well so that it is absorbed and let it dry.\n" +
+                        "Repeat the application two to three times a day. It will be very useful in case you sweat excessively"
+
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.pemp,
+                "Pemphigus",
+                "Serious skin disease",
+                "\nCoconut oil is useful in Herbal Treatment for Bullous Pemphigoid skin condition " +
+                        "as they are full with fatty acids can lessen the inflammation throughout the body. " +
+                        "Starting from coconut oil, it has anti-bacterial, anti-fungal and anti-viral agents with healing effect." +
+                        " This is why it makes sense to utilize coconut oil for your pemphigus sufferings"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.lorry,
+                "Leprosy",
+                "There is Tow Herba",
+                "\n1-Chalmoorga oil is also helpful in preventing leprosy." +
+                        " Make paste of chalmoorga oil with lemon juice and then apply this paste on the affected area. " +
+                        "This mixture will surely help the patient to get rid from pains and inflammation. This mixture has a cooling effect also."+
+                        "\n2-Gota kola which is also known as Cantella Asiatica, Hydrocotyle is helpful in this regard. " +
+                        "Gota kola tea is very effective for patients for getting relief from pain and fever. " +
+                        "You can also apply the juice of gota kola herb on the affected area. For this boil some pieces of cloth and then rinse those pieces in gota kola juice." +
+                        " You can apply these pieces of cloth as bandage on the affected area. Gota kola herb is also available in market in powder and ointment form. You can even eat its powder with rice for relief."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
                 R.drawable.hrbeczema,
                 "Eczema",
-                "Apple cider vinegar Always dilute apple cider vinegar before applying it to the skin — undiluted vinegar can cause chemical burns or other injuries.\n" +
-                        "\n" +
-                        "People can use the vinegar in wet wraps or baths, and it is available in most supermarkets and health stores.",
+                "Apple cider vinegar",
                 "\nTo use apple cider vinegar in a wet wrap:\n" +
                         "\n" +
                         "Mix 1 cup of warm water and 1 tablespoon of apple cider vinegar.\n" +
@@ -40,6 +106,23 @@ object SetDataHerbs {
                         "Turmeric is an effective natural remedy for many skin conditions because of its antimicrobial and anti-inflammatory prowess. Its antifungal properties can help treat tinea versicolor and prevent the infection from spreading.\n" +
                         "\n" +
                         "Mix a teaspoon of turmeric powder with a little water and mix it into a nice, smooth paste. Apply the finished paste to the affected parts of your skin and leave it dry for at least 30 minutes before rinsing it off.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.scable,
+                "Scabies",
+                "Natural Herbs",
+                "\n1-The oldest and the most effective treatment for scabies is ; " +
+                        "the application of a paste, prepared by mixing two teaspoons of sublimed sulphur with 8 tablespoons of coconut oil." +
+                        " After bathing with some mild soap, " +
+                        "apply this solution on overall body. Leave it for 5-7 minutes which will help to soak the medicine."+
+                        "\n2-The juice of fresh apricot leaves is a valuable remedy. You can apply it on the affected area. " +
+                        "twice a day for better results."+
+                        "\n3-One should also take the juice of bitter gourd mixed with lime juice;" +
+                        " once a day. Within a week, it starts showing the results."
+
             )
         )
 
@@ -91,28 +174,7 @@ object SetDataHerbs {
             )
         )
 
-        herbsData.add(
-            DataUsed(
-                R.drawable.hrbhairloss,
-                "Hair Loss",
-                "Ginkgo hair mask",
-                "\nIngredients:\n" +
-                        "1 tsp ginkgo biloba powder\n" +
-                        "1 chopped banana\n" +
-                        "2 tsp coconut oil\n" +
-                        "1 tsp honey\n" +
-                        "Steps:\n" +
-                        "Mix all the ingredients and apply this mask to your hair.\n" +
-                        "Leave it on for half an hour, and then rinse it off with a mild shampoo." +
-                        "2-Onions are widely acknowledged for their hair growing potential. They are loaded with sulfur, which is one of the most important hair-building nutrients. \n" +
-                        "\n" +
-                        "The juice extracted from onions contains concentrated amounts of sulfur, which when applied to the scalp helps stimulate hair follicles and promote hair regrowth. Moreover, onions are known to exhibit antibacterial properties that help fight against scalp infections.       " +
-                        "                   how to make it :                                                                                                                                                                                                                                  Grind one onion and squeeze out its juice.\n" +
-                        "Soak a cotton ball in the onion juice and dab it all over your scalp.\n" +
-                        "Let your scalp marinate in the juice for 20–30 minutes, and then wash it off using a mild shampoo and plain water.\n" +
-                        "Use this hair treatment once a week.\n\n"
-            )
-        )
+
 
         herbsData.add(
             DataUsed(
@@ -165,11 +227,99 @@ object SetDataHerbs {
 
         herbsData.add(
             DataUsed(
+                R.drawable.rash,
+                "Rash",
+                "Aloe Vera",
+                "\nApply after showering and allow it to dry. " +
+                        "\nThis is one of the best treatments for rashes related to poison ivy."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.smallbox,
+                "Smallpox",
+                "2-A paste of Red sandalwood ",
+                "\npowder with water is one of the most popular home remedies for smallpox. \n" +
+                        "\n" +
+                        "Paste provides instant relief from itching and burning and heals blisters faster."+
+                        "\nPoppy seeds are home remedies for smallpox which help in suppressing the virus and its activities and relieving symptoms. The blisters of smallpox can cause severe burning and itching.\n" +
+                        "\n" +
+                        "The paste of poppy seeds, when applied over blisters, provides cooling effects. " +
+                        "It relieves itching and burning and helps in healing blisters faster."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.imprti,
+                "Impetigo",
+                "Garlic & Ginger ",
+                "\nGarlic To use this remedy: Place the cut side of a slice of garlic directly on impetigo sores. This may sting a little. You can also press garlic cloves, and then apply topically. Garlic is also great to incorporate into your diet.\n" +
+                        "\n" +
+                        "Avoid using garlic on young children, as it may cause skin irritation."+
+                        "\nGinger\n" +
+                        "To use this remedy: Place a slice of ginger, cut side down, on impetigo sores. It might sting a little. You can also juice ginger root and make a poultice from the juice, applying it topically. Incorporating ginger into your diet is another option.\n" +
+                        "\n" +
+                        "Avoid using ginger on young children, as it may cause skin irritation."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
                 R.drawable.hrbvas,
                 "vasculitis",
                 "Apple Cider Vinegar",
                 "\nACV is a wonderful natural remedy for many of the vasculitis symptoms, " +
                         " The acetic and malic acids in ACV have antiviral, antibiotic and anti-fungal properties.\n\n"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.system,
+                "systemic",
+                "Neem & Chamomile",
+                "\nNeem \n" +
+                        "Neem oil contains the necessary fatty acids, promoting wound healing and making your skin healthy. " +
+                        "Neem also has anti-inflammatory properties that reduce Acne. " +
+                        "The neem oil is believed to relieve skin dryness, skin itchiness, and redness. It also prevents pimples and skin blemishes."+
+                        "\n\nChamomile\n" +
+                        "Using chamomile oil on the skin brings along powerful antioxidants that help the skin regenerate, tighten pores, and slow down aging effects on the skin." +
+                        " Blemishes and redness can be lightened by chamomile, sometimes referred to as 'natural skin bleach'."
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.insect,
+                "insecets bite and sting",
+                "Apple Cider Vinegar",
+                "\nFresh plantain leaf can be used to provide immediate relief for insect bites and stings. " +
+                        "To make a poultice, fresh plantain leaf can be mixed with bentonite clay and water to form a paste. " +
+                        "Alternatively, a leaf can be chewed and placed directly over the bite."+
+                        "\n2-Calendula (Calendula officinalis)\n" +
+                        "Calendula oil or fresh leaves can soothe irritated, itchy skin, and can encourage healthy healing of bites and stings. A simple salve can be created with calendula, beeswax, and antiseptic essential oils, such as tea tree, rosemary, and lavender."+
+                        "\n3-Comfrey (Symphytum officinale)\n" +
+                        "Comfrey infused oil or fresh juice from leaves can be used topically for many skin conditions, including rashes, scrapes, and wounds. For bites and stings, it can soothe itching and irritation."+
+                        "\n4-Aloe Vera\n" +
+                        "If you have an aloe Vera at home then just pluck out a leaf. Peel it and extract the gel from it. Apply this gel to the area of the insect bite.\n" +
+                        "\n" +
+                        "Aloe Vera takes the sting out of the bite and at the same time hydrates the skin. The longer it is kept, the better and faster it works. One can also use the gel that is available in tubes at the local chemist."+
+                        "\n5-Chamomile\n" +
+                        "Chamomile herbal tea bags can be used to get relief from insect bites. Take one chamomile tea bag, one cup boiling water and one cup ice water. Dip the tea bag into the boiling water for a couple of minutes. Squeeze the tea bag and dip it in the ice water again for a couple of minutes. Now squeeze the tea bag again to remove all the excess water and use the tea bag on the affected area. " +
+                        "It will soothe and help remove the inflammation"
+            )
+        )
+
+        herbsData.add(
+            DataUsed(
+                R.drawable.wartst,
+                "warts molluscum",
+                "Garlic",
+                "\nGarlic has been shown to have antibacterial, anti-fungal and antiviral properties that is useful in treating warts. There are two ways of applying this garlic extract, one is lipid and other is water extract.\n" +
+                        "\n" +
+                        "\n80 % of people applied lipid garlic extract for two weeks and their warts vanished completely. Raw, crushed or fresh garlic oil can be directly applied to the warts of hands or feet..\n\n"
             )
         )
 
